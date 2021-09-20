ee482a484ea3cadc9704e5a4f52b087bee20f26c
Thu Ha <hathudo9910@gmail.com>
2021-09-20 20:56:24 +0700

    bai1

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# anhthanhcute
-trungthuzuizenheanhthanh
+# FirstRepo
---
+++ main.js
@@ -0,0 +1,6 @@
+var fullName = 'Đỗ Thu Hà';//đặt biến!!
+var tuoi = 19;
+var remind='Học triết đi';
+alert(fullName);//thông báo
+alert(tuoi);
+alert(remind);
